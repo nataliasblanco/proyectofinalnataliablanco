470a75d7a1f1ad558039629e3094659408cad01c
NataliaBlanco <nataliasblanco@hotmail.com>
2023-11-10 20:24:09 -0300

    inclusión del form y la bienvenida

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 const hamburguesa = document.querySelector('.hamburguesa');
+const navegacion = document.querySelector('.navegacion');
+const enlaces = document.querySelectorAll('.navegacion a');
 
-console.log(hamburguesa)
+document.addEventListener('DOMContentLoaded',()=>{
+    mostrarMenu();
+    cerrarMenu();
+});
+
+function mostrarMenu(){
+    hamburguesa.addEventListener('click',()=>{
+        navegacion.classList.toggle('ocultar');
+
+    });
+}
+
+function cerrarMenu(){
+    enlaces.forEach(enlace =>{
+        enlace.addEventListener('click',(e)=>{
+
+            if(e.target.tagName === 'A'){
+                navegacion.classList.add('ocultar');
+            }
+        });
+    });
+}
