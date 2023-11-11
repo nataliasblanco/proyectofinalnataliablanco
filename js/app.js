60142ac8671f30b1142dc763d4405ffd3d1ba818
NataliaBlanco <nataliasblanco@hotmail.com>
2023-11-11 00:36:54 -0300

    últimos cambios al 11 de noviembre

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 const hamburguesa = document.querySelector('.hamburguesa');
 const navegacion = document.querySelector('.navegacion');
 const enlaces = document.querySelectorAll('.navegacion a');
-
+const fecha = document.querySelector('.fecha');
 document.addEventListener('DOMContentLoaded',()=>{
     mostrarMenu();
     cerrarMenu();
+    fechaActual();
 });
 
 function mostrarMenu(){
@@ -17,10 +18,23 @@ function mostrarMenu(){
 function cerrarMenu(){
     enlaces.forEach(enlace =>{
         enlace.addEventListener('click',(e)=>{
-
+            e.preventDefault();
+            const seccion = document.querySelector(e.target.attributes.href.value);
+            cambioSeccion(seccion);
             if(e.target.tagName === 'A'){
                 navegacion.classList.add('ocultar');
             }
         });
     });
+}
+function cambioSeccion(seccion){
+    seccion.scrollIntoView({
+        behavior:'smooth'
+    })
+}
+
+
+function fechaActual(){
+    let fechaHoy = new Date().getFullYear();
+    fecha.textContent = fechaHoy;
 }
